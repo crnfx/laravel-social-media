acc75bcd27643a10bc18984755cf516102051ae8
Roman <roma.bench@yandex.ru>
2025-12-02 20:21:03 +0300

    feat(AttachmentsMigration): update migration, add additional columns

--- database/migrations/2025_10_18_173753_create_attachments_table.php
+++ database/migrations/2025_10_18_173753_create_attachments_table.php
@@ -13,8 +13,18 @@ return new class extends Migration
     {
         Schema::create('attachments', function (Blueprint $table) {
             $table->id();
-            $table->string('path')->nullable();
+            $table->foreignId('post_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('path');
+            $table->string('original_name');
+            $table->string('mime_type');
+            $table->integer('size')->default(0);
+            $table->integer('downloads')->default(0);
             $table->timestamps();
+            
+            $table->index('post_id');
+            $table->index('user_id');
+            $table->index('mime_type');
         });
     }
 
